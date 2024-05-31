6433000696955427c6295de7b5270e2a113f0195
Exploriter <67083853+gigawatson@users.noreply.github.com>
2024-05-31 09:06:18 -0700

    Prune

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,6 @@ module.exports = {
         'averia': ['"Averia Serif Libre"', ...defaultTheme.fontFamily.serif],
         'ibmplex': ['"IBM Plex Mono"', ...defaultTheme.fontFamily.mono],
         'vollkorn': ['"Vollkorn"', ...defaultTheme.fontFamily.serif],
-        'inter': ['"Inter"', ...defaultTheme.fontFamily.sans],
-        'spacegrotesk': ['"Space Grotesk"', ...defaultTheme.fontFamily.mono],
         'ebgaramond': ['"EB Garamond"', ...defaultTheme.fontFamily.serif],
         'pirataone': ['"Pirata One"', ...defaultTheme.fontFamily.serif],
       },
