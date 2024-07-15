f1b8e6b4613d0fe7cf23df9c4b74675a0859fee0
Exploriter <67083853+gigawatson@users.noreply.github.com>
2024-07-14 20:06:04 -0700

    Lexend is actually very ugly I don't know what I was thinking

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  darkMode: 'selector',
-  content: [
-    './**/index.html',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'averia': ['"Averia Serif Libre"', ...defaultTheme.fontFamily.serif],
-        'ibmplex': ['"IBM Plex Mono"', ...defaultTheme.fontFamily.mono],
-        'vollkorn': ['"Vollkorn"', ...defaultTheme.fontFamily.serif],
-        'lexend': ['"Lexend"', ...defaultTheme.fontFamily.sans],
-        'pirataone': ['"Pirata One"', ...defaultTheme.fontFamily.serif],
-      },
+    darkMode: 'selector',
+    content: [
+        './**/index.html',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'averia': ['"Averia Serif Libre"', ...defaultTheme.fontFamily.serif],
+                'ibmplex': ['"IBM Plex Mono"', ...defaultTheme.fontFamily.mono],
+                'vollkorn': ['"Vollkorn"', ...defaultTheme.fontFamily.serif],
+                'pirataone': ['"Pirata One"', ...defaultTheme.fontFamily.serif],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 
